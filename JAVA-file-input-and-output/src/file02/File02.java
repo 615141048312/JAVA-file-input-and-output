4dc510d6a581c6f9e9b11233811ace1747fb3eb2
615141048312 <hanada.yui@mbr.nifty.com>
2020-09-05 23:51:21 +0900

    file2

---
+++ JAVA-file-input-and-output/src/file02/File02.java
@@ -0,0 +1,20 @@
+package file02;
+
+import java.io.File;
+
+public class File02 {
+	public static void main(String[] args) {
+		File desktop = new File("../");
+		
+		File filelist[] = desktop.listFiles();
+		for (int i = 0; i < filelist.length; i++) {
+			if (filelist[i].isFile()) {
+				System.out.println("[F]" + filelist[i].getName());
+			} else if (filelist[i].isDirectory()) {
+				System.out.println("[D]" + filelist[i].getName());
+			} else {
+				System.out.println("[?]" + filelist[i].getName());
+			}
+		}
+	}
+}
