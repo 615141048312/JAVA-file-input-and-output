50b1268f61432640ea211786c86628a4911f920c
615141048312 <hanada.yui@mbr.nifty.com>
2020-09-11 14:58:24 +0900

    finish InputOutput01

---
+++ JAVA-file-input-and-output/src/inputOutput/InputOutput01.java
@@ -0,0 +1,26 @@
+package inputOutput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputOutput01 {
+	public static void main(String[] args) {
+		try {
+			File file = new File("newfile.txt");
+			FileReader filereader = new FileReader(file);
+			
+			int ch;
+			while((ch = filereader.read()) != -1) {
+				System.out.print((char)ch);
+			}
+			
+			filereader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
