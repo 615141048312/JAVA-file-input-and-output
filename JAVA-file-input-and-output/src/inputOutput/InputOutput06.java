a693b2e49cb9ab94a0add8886b3eeedcb3752065
615141048312 <hanada.yui@mbr.nifty.com>
2020-09-15 16:15:33 +0900

    finish 06

---
+++ JAVA-file-input-and-output/src/inputOutput/InputOutput06.java
@@ -0,0 +1,39 @@
+package inputOutput;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class InputOutput06 {
+	public static void main(String[] args) {
+		try {
+			File file = new File("/Users/yui/Desktop/newDirectory/file.txt");
+			
+			if (checkBeforeWritefile(file)) {
+				BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+				
+				bw.write("こんにちは");
+				bw.newLine();
+				bw.write("お元気ですか？");
+				bw.newLine();
+				
+				bw.close();
+			} else {
+				System.out.println("ファイルに書き込めません");
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+	
+	private static boolean checkBeforeWritefile(File file) {
+		if (file.exists()) {
+			if (file.isFile() && file.canWrite()) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
